b73481212e810ac5902f72145929dce33921bf13
nilesh65 <nileshmukherjee65@gmail.com>
2023-09-08 21:01:45 +0530

    lecture-10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 const App = () => {
   return (
-    <div>Welcome To ProShop</div>
-  )
-}
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+      <Outlet />
+    </Container>
+    </main>
+    <Footer />
+    </>
+
+  );
+};
 
 export default App
